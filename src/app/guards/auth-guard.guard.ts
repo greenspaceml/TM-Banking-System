e0024c79b69cffa110348f48aa0a8fa5fd259c38
Quang Hoang <quanghoang@Quangs-MacBook-Pro.local>
2021-12-28 00:34:08 +0700

    update guard

---
+++ src/app/guards/auth-guard.guard.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  CanActivateChild,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuardGuard implements CanActivate, CanActivateChild {
+  constructor(private router: Router) {}
+  canActivateChild(
+    childRoute: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | boolean
+    | UrlTree
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree> {
+    if (!localStorage.getItem('isLoggin')) {
+      this.router.navigate(['/login']);
+      return false;
+    }
+    return true;
+  }
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    if (!localStorage.getItem('isLoggin')) {
+      this.router.navigate(['/login']);
+      return false;
+    }
+    return true;
+  }
+}
